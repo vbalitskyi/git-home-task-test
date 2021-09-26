6f3827a91442823c359c8322357a8fb4eb1cabd7
qabal <qabal@DESKTOP-414DRDL>
2021-09-26 12:46:39 +0300

    26,09

---
+++ Task 1/Lesson2/Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() { GroupName = "LP1", Name = "Павло" };
+            Student student2 = new Student() { GroupName = "LP2", Name = "Катерина" };
+
+            Pupil pupil1 = new Pupil() { ClassName = "1B", Name = "Денис" };
+            Pupil pupil2 = new Pupil() { ClassName = "2B", Name = "John" };
+
+            Student[] students = new Student[10];
+            students[0] = student1;
+            students[1] = student2;
+            
+
+
+            students[2] = new Student() { GroupName = "LP4", Name = "John" };
+
+            Console.Read();
+        }
+    }
+}
---
+++ Task 1/Lesson2/Lesson2/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Student: IHuman
+    {
+        public string GroupName { get; set; }
+        public string Name { get; set; }
+    }
+}
